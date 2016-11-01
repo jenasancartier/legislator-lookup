1d86a3f886aac4af0cf85d1e45f59c7b9b299c74
Jena SanCartier <jenasancartier@gmail.com>
2016-11-01 09:23:54 -0700

    follow along with homework: hide api key, store it as environment variable

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,8 +1,10 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=5eae2b909c6d4fabbf37d20b82fa738b&zip=' + params.zip;
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +key+ '&zip=' + params.zip;
     return Ember.$.getJSON(url).then(function(responseJSON) {
       return responseJSON.results;
     });
